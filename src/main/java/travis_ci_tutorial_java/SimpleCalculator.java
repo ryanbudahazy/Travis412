f8123ed7cdcf9e8a69fdb0ac16cdc8c82389cd86
mcque <mcque@10.0.0.186>
2020-12-01 21:38:21 -0500

    Commit test 13.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -11,6 +11,12 @@ public class SimpleCalculator {
 		return a * b;
 	}
 	public int divide(int a, int b) {
-		return a / b;
+		if (b != 0) {
+			return a / b;
+		}
+		
+		else {
+			return 999;
+		}
 	}
 }
